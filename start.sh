4aa0c5c558e96ffbfb5a6cef117e1ee91cc6b238
BenzoMusic <rrodtang@gmail.com>
2025-02-18 21:52:10 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Создаем виртуальное окружение
+python3 -m venv venv
+
+# Активируем виртуальное окружение
+source venv/bin/activate
+
+# Устанавливаем зависимости
 pip install -r requirements.txt
+
+# Запускаем приложение
 uvicorn app:app --host 0.0.0.0 --port 10000
