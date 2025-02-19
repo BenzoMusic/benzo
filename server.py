bc95c278d8d6e0b0452f004aae5ab0ebfa0c880f
BenzoMusic <rrodtang@gmail.com>
2025-02-19 01:01:19 +0000

    deb

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
+from flask_cors import CORS  # Импортируем CORS
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+CORS(app)  # Включаем CORS для всего приложения
+socketio = SocketIO(app, cors_allowed_origins="*")  # Разрешаем все источники для SocketIO
 
 # Текущее состояние плеера
 player_state = {
